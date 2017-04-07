b04f0b666809cb2fcb0f049087bcd785397a3aab
Theodor Vararu <theo@vararu.org>
2017-04-07 17:23:03 +0100

    Configure and use accessible-typeahead for pattern-search

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -9,4 +9,57 @@ $(function() {
       lunrIndex = lunr.Index.load(lunrData.index);
     }
   });
+
+  function typeaheadSource (searchTerms, syncResults) {
+    if (lunrIndex) {
+      var lunrResults = lunrIndex.search(searchTerms);
+      syncResults(lunrResults)
+    } else {
+      syncResults([])
+    }
+  }
+
+  function typeaheadOnSelect (lunrResult) {
+    if (lunrResult) {
+      var resultData = lunrData['docs'][lunrResult.ref];
+      window.location.href = resultData['url']
+    }
+  }
+
+  // The value that will be inserted into the `<input>` field when the user confirms.
+  function inputValueTemplate (lunrResult) {
+    return lunrResult
+      ? lunrData['docs'][lunrResult.ref]['title']
+      : ''
+  }
+
+  function suggestionTemplate (lunrResult) {
+    var resultData = lunrData['docs'][lunrResult.ref];
+    var breadcrumb = resultData['section'];
+
+    if (resultData['theme']) {
+      breadcrumb = breadcrumb + " > " + resultData['theme']
+    }
+
+    var resultContent = $('<div>', {class: 'result-inner'}).text(resultData['title'])
+      .append(
+        $('<div>', {class: 'result-section'}).text(breadcrumb)
+      )
+
+    return resultContent.prop('outerHTML')
+  }
+
+  AccessibleTypeahead({
+    displayMenu: 'overlay',
+    element: document.getElementById('search-typeahead'),
+    onSelect: typeaheadOnSelect,
+    placeholder: 'Search patterns',
+    selectOnBlur: false,
+    showNoOptionsFound: false,
+    source: typeaheadSource,
+    templates: {
+      inputValue: inputValueTemplate,
+      suggestion: suggestionTemplate
+    }
+  });
 });
