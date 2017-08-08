40caed081078b87ee023677ceacfca696c1b3314
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2017-08-08 16:21:13 +0100

    Fix link to view history for a page

--- helpers/github_helpers.rb
+++ helpers/github_helpers.rb
@@ -3,13 +3,23 @@ module GithubHelpers
   def issues_link(title = nil)
     if title
       label = title.downcase.strip.gsub(' ', '+').gsub(/[^\w\+]/, '')
-      "https://github.com/alphagov/govuk_design_prototypes/issues?q=is%3Aissue+is%3Aopen+label%3A%22#{label}%22"
+      "#{repo_root}/issues?q=is%3Aissue+is%3Aopen+label%3A%22#{label}%22"
     else
-      "https://github.com/alphagov/govuk_design_prototypes/issues?q=is%3Aissue+is%3Aopen"
+      "#{repo_root}/issues?q=is%3Aissue+is%3Aopen"
     end
   end
 
   def edit_link
-    "https://github.com/alphagov/govuk_design_prototypes/blob/master/source/#{current_page.file_descriptor.normalized_relative_path}"
+    "#{repo_root}/blob/master/source/#{current_page.file_descriptor.normalized_relative_path}"
+  end
+
+  def history_link
+    "#{repo_root}/commits/master/source/#{ current_page.file_descriptor.normalized_relative_path }"
+  end
+
+private
+
+  def repo_root
+    "https://github.com/alphagov/govuk_design_prototypes"
   end
 end
